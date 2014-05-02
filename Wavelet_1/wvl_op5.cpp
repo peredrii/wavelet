84726dcc0d4a022c83ca558b253122cd66a0a34b
OP5 <krakoryak@gmail.com>
2014-05-03 01:58:05 +0400

    second time

--- Wavelet_1/wvl_op5.cpp
+++ Wavelet_1/wvl_op5.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <glut.h>
 #include <math.h>
@@ -6,23 +5,60 @@
 
 #define double float
 
-double CL[4] = {(1 + sqrt(3.)) / (4 * sqrt(2.)),
-                (3 + sqrt(3.)) / (4 * sqrt(2.)),
-                (3 - sqrt(3.)) / (4 * sqrt(2.)),
-                (1 - sqrt(3.)) / (4 * sqrt(2.))};
-double CH[4] = {(1 - sqrt(3.)) / (4 * sqrt(2.)),
-               -(3 - sqrt(3.)) / (4 * sqrt(2.)),
-                (3 + sqrt(3.)) / (4 * sqrt(2.)),
-               -(1 + sqrt(3.)) / (4 * sqrt(2.))}; 
+double CL[4] = {(1 + sqrt(3.)) / (4 * sqrt(2.)),    // c0
+                (3 + sqrt(3.)) / (4 * sqrt(2.)),    // c1
+                (3 - sqrt(3.)) / (4 * sqrt(2.)),    // c2
+                (1 - sqrt(3.)) / (4 * sqrt(2.))};   // c3
+double CH[4] = {(1 - sqrt(3.)) / (4 * sqrt(2.)),    // c3
+               -(3 - sqrt(3.)) / (4 * sqrt(2.)),    // -c2
+                (3 + sqrt(3.)) / (4 * sqrt(2.)),    // c1
+               -(1 + sqrt(3.)) / (4 * sqrt(2.))};   // -c0
+double OCL[4] = {(3 - sqrt(3.)) / (4 * sqrt(2.)),   // c2
+                 (3 + sqrt(3.)) / (4 * sqrt(2.)),   // c1
+                 (1 + sqrt(3.)) / (4 * sqrt(2.)),   // c0
+                 (1 - sqrt(3.)) / (4 * sqrt(2.))};  // c3                
+double OCH[4] = {(1 - sqrt(3.)) / (4 * sqrt(2.)),   // c3                
+                -(1 + sqrt(3.)) / (4 * sqrt(2.)),   // -c0
+                 (3 + sqrt(3.)) / (4 * sqrt(2.)),   // c1
+                -(3 - sqrt(3.)) / (4 * sqrt(2.))};  // -c2
 typedef struct
 {
   int W, H;
   double *Pix;
 } PIC;
 
-/* Glibal image and waveletted image */
+/* Global image and waveletted image */
 PIC WorkPic, WavePic;
 
+/* Min value */
+float Min( float *data, int N )
+{
+  float minval = data[0];
+
+  for (int i = 0; i < N - 1; i++)
+  {
+    if (minval > data[i + 1])
+      minval = data[i + 1];
+  }
+
+  return minval;
+}
+
+float Max( float *data, int N )
+{
+  float maxval;
+
+  maxval = data[0];
+  for (int i = 0; i < N - 1; i++)
+  {
+    if (maxval < data[i+1])
+      maxval = data[i+1];
+  }
+
+  return maxval;
+}
+
+/* Creating picture: allocate memory, initialize W, H */ 
 int Create( PIC *P, int W, int H )
 {
   P->W = P->H = 0;
@@ -35,6 +71,7 @@ int Create( PIC *P, int W, int H )
   return 1;
 }
 
+/* Load picture from file using filename */
 int Load( PIC *P, char *FileName )
 {
   FILE *F;
@@ -162,7 +199,9 @@ int Load( PIC *P, char *FileName )
   fclose(F);
   return 1;
 }
-
+         
+/* Coefficient to high-freq-filter
+ * NOT USED */
 double* hpf_coeffs( double *Cl )
 {
   int N = sizeof(Cl)/sizeof(*Cl);
@@ -178,6 +217,7 @@ double* hpf_coeffs( double *Cl )
   return CHZ;
 }
 
+/* Pair convolution. Dimensional transform */
 double* pconv( double *arr, int num, double *Cl, double *Ch)
 {
   double* out = (double *)malloc(sizeof(double) * num);
@@ -201,6 +241,7 @@ double* pconv( double *arr, int num, double *Cl, double *Ch)
   return out;
 }
 
+/* 2-dimensional transform */
 PIC dwt2( PIC *P, double *Cl, double* Ch )
 {
   PIC outpic, resoutpic;
@@ -208,18 +249,17 @@ PIC dwt2( PIC *P, double *Cl, double* Ch )
 
   Create(&outpic, P->W, P->H);
   Create(&resoutpic, P->W, P->H);
+  
   /* handle rows */
-
   for (int i = 0; i < P->H; i++)
   {
     p = pconv(&P->Pix[i * P->W], P->W, Cl, Ch);
     for (int j = 0; j < P->W; j++)
       outpic.Pix[i * P->W + j] = p[j]; //* 0.5;
     free(p);
-  }
+  }                      
 
-  /* handle columns*/
-
+  /* handle columns*/      
   for (int i = 0; /*0 && */i < P->W; i++)
   {
     double *v = (double *)malloc(sizeof(double) * P->H);
@@ -232,6 +272,13 @@ PIC dwt2( PIC *P, double *Cl, double* Ch )
     free(v);
   }
 
+  /*
+  printf("Min\n");
+  printf("%.16f\n", Min(outpic.Pix, outpic.H * outpic.W));
+  printf("Max\n");
+  printf("%.16f\n", Max(outpic.Pix, outpic.H * outpic.W));
+  */
+
   /* 0..h/2, 0..w/2 */
   for (int i = 0, k = 0; i < outpic.H / 2, k < outpic.H; i++, k += 2)
   {
@@ -241,32 +288,35 @@ PIC dwt2( PIC *P, double *Cl, double* Ch )
     }
   }
 
-  /* h/2..h, 0..w/2 */
-  for (int i = outpic.H / 2, k = 1; i < outpic.H, k < outpic.H; i++, k += 2)
-  {
-    for (int j = 0, l = 0; j < outpic.W / 2, l < outpic.W; j++, l += 2)
-    {
-      resoutpic.Pix[i * outpic.W + j] = outpic.Pix[k * outpic.W + l];         
-    }
-  }
+  ///* h/2..h, 0..w/2 */
+  //for (int i = outpic.H / 2, k = 1; i < outpic.H, k < outpic.H; i++, k += 2)
+  //{
+  //  for (int j = 0, l = 0; j < outpic.W / 2, l < outpic.W; j++, l += 2)
+  //  {
+  //    resoutpic.Pix[i * outpic.W + j] = outpic.Pix[k * outpic.W + l];         
+  //  }
+  //}
 
-  /* 0..h/2, w/2..w */
-  for (int i = 0, k = 0; i < outpic.H / 2, k < outpic.H; i++, k += 2)
-  {
-    for (int j = outpic.W / 2, l = 1; j < outpic.W, l < outpic.W; j++, l += 2)
-    {
-      resoutpic.Pix[i * outpic.W + j] = outpic.Pix[k * outpic.W + l];         
-    }
-  }
+  ///* 0..h/2, w/2..w */
+  //for (int i = 0, k = 0; i < outpic.H / 2, k < outpic.H; i++, k += 2)
+  //{
+  //  for (int j = outpic.W / 2, l = 1; j < outpic.W, l < outpic.W; j++, l += 2)
+  //  {
+  //    resoutpic.Pix[i * outpic.W + j] = outpic.Pix[k * outpic.W + l];         
+  //  }
+  //}
 
-  /* h/2..h, w/2..w */
-  for (int i = outpic.H / 2, k = 1; i < outpic.H, k < outpic.H; i++, k += 2)
-  {
-    for (int j = outpic.W / 2, l = 1; j < outpic.W, l < outpic.W; j++, l += 2)
-    {
-      resoutpic.Pix[i * outpic.W + j] = outpic.Pix[k * outpic.W + l];         
-    }
-  }
+  ///* h/2..h, w/2..w */
+  //for (int i = outpic.H / 2, k = 1; i < outpic.H, k < outpic.H; i++, k += 2)
+  //{
+  //  for (int j = outpic.W / 2, l = 1; j < outpic.W, l < outpic.W; j++, l += 2)
+  //  {
+  //    resoutpic.Pix[i * outpic.W + j] = outpic.Pix[k * outpic.W + l];         
+  //  }
+  //}
+
+  //for (int i = 0; i < resoutpic.W * resoutpic.H; i++)
+    //resoutpic.Pix[i] = (resoutpic.Pix[i] + 1) / (resoutpic.Pix[i] + 2);
 
   return resoutpic;
 }
@@ -283,6 +333,7 @@ void Display( void )
   glFinish();
   glutSwapBuffers();
 }
+
 void main( void )
 {
   Load(&WorkPic, "4.bmp");
